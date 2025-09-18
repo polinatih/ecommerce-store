b4b51b0c8bb4b82426231d4db15041fae45b4666
Polina <tihaninap@gmail.com>
2025-09-18 12:02:05 +0300

    environment setup

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,24 @@
-const HomePage = () => {
-    return (
-        <div> HellloStore</div>
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true});
+    const billboard = await getBillboard("718d11c8-c80a-4e87-9e9f-8967dc111efc");
+    
+    return(
+        <Container>
+            <div className="space-y pb-10">
+                <Billboard data={billboard} />
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
+            </div>
+        </Container>
     )
 }
 
